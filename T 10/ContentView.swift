cad85c295f9464287a28f0c40f48e444482ef8b4
MaramRAlanezi <malanezi24@twq.idserve.net>
2024-09-29 10:21:35 +0300

    For test

--- T 10/ContentView.swift
+++ T 10/ContentView.swift
@@ -10,10 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image("Maram")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                .frame(width: 150, height:150)
+                .clipped(antialiased:Bool())
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                .overlay(Circle().stroke(Color.yellow, lineWidth: 5))
+            
+                .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Maram Alanezi")
+                .font(.largeTitle)
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                .foregroundColor(Color.red)
+                .padding(.leading, -100)
+            
+            Text("It's Maram Health Informatics Specialist I enjoy reading, bike riding and photography. These activities provide me with a balance between work and personal growth, while also nurturing my creativity and love for exploration.")
         }
         .padding()
     }
