0c52a73473ea2acfd996996781907c10dd64bb53
codelab <malanezi24@twq.idserve.net>
2024-09-29 09:26:49 +0300

    Initial Commit

---
+++ T 10/T_10App.swift
@@ -0,0 +1,17 @@
+//
+//  T_10App.swift
+//  T 10
+//
+//  Created by Maram Rabeh  on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct T_10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
